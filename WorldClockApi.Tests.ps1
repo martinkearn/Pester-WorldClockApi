ad3b499da3100141c43a509fc49b347d6f5f0045
Martin Kearn <martin.kearn@microsoft.com>
2021-02-18 10:15:20 +0000

    Update WorldClockApi.Tests.ps1

--- WorldClockApi.Tests.ps1
+++ WorldClockApi.Tests.ps1
@@ -2,36 +2,36 @@ Describe 'Test worldclockapi.com' {
     BeforeAll {
         $response = Invoke-WebRequest -Method 'GET' -Uri 'http://worldclockapi.com/api/json/utc/now'
         $responseContent = $response.Content | ConvertFrom-Json
-        $dayOfWeek = (Get-Date).DayOfWeek
-        $year = Get-Date -Format "yyyy"
-        $month = Get-Date -Format "MM"
     }
 
     It "It should respond with 200" {
         $response.StatusCode | Should -Be 200
     }
-
+    
     It "It should have a null service response" {
         $responseContent.serviceResponse | Should -BeNullOrEmpty
     } 
-
-    It "It should be $dayOfWeek" {
+    
+    It "It should be the right day of the week" {
+        $dayOfWeek = (Get-Date).DayOfWeek
         $responseContent.dayOfTheWeek | Should -Be $dayOfWeek
     }
-
-    It "It should be year $year" {
+    
+    It "It should be the right year" {
+        $year = Get-Date -Format "yyyy"
         $responseContent.currentDateTime | Should -BeLike "*$year*"
     }
-
-    It "It should be month $month" {
+    
+    It "It should be the right month" {
+        $month = Get-Date -Format "MM"
         $responseContent.currentDateTime | Should -BeLike "*$month*"
     }
-
-    # Assumes you are running this outside daylight savings .. hacky but good way to showcase the syntax ;)
+    
+    # These two tests assume you are running this outside daylight savings (during the winter) .. hacky but good way to showcase the syntax ;)
     It "It should not be daylight savings time" {
         $responseContent.isDayLightSavingsTime | Should -Not -Be $true
     }
-
+    
     It "It should not be daylight savings time another way" {
         $responseContent.isDayLightSavingsTime | Should -BeFalse
     }
